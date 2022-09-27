93b34c0d2085a892dbfccb9186f3a29616bf7d4f
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2022-09-26 21:43:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const { request } = require('http');
+const { response } = require('express');
+
+const app = express();
+
+app.listen(5500, ()=>{
+    console.log ('server runing 5500 🚀')
+});
+
+app.use(cors())
+
+app.use(express.json())
+
+let users = [
+    {
+        id:1,
+        name: "Bruna Yumi",
+        city: "São Paulo",
+        country: "Brazil"
+
+    }
+]
+
+app.route('/api').get((req, res) => res.json({
+    users
+  }))
+  
+app.route('/api/:id').get((request, response) => {
+    const userId = request.params.id;
+
+    const user = users.find(user => Number(user.id)===Number(userId));
+
+    if (!user){
+        return response.json('User not found!')
+    }
+
+    response.json(user)
+})
+
+app.route('/api').post((request, response) => {
+    const lastId = users[users.length - 1].id;
+
+    users.push({
+        id:lastId + 1,
+        name: request.body.name,
+        city: request.body.city,
+        country: request.body.country
+    })
+
+    response.json('Saved user')
+})
+
+app.route('/api/:id').put((request, response) =>{
+    const { userId } = request.params;
+
+    const user = users.find(user => Number(user.id) === Number(userId));
+    
+    if (!user){
+        return response.json('User not found!')
+    }
+
+    const updateUser = {
+        ...user,
+        name: request.body.name,
+        city: request.body.city,
+        country: request.body.country
+    }
+
+    users = users.map(user => {
+        if(Number(user.id) === Number(userId)) {
+            user = updateUser
+        }
+        return user
+    })
+    response.json("Updated user")
+});
+
+app.route('api/:id').delete((request, response) => {
+    const { userId } = request.params;
+    users.filter = (user => Number(user.id) !== Number(userId));
+
+    response.json('Deleted User!')
+})
